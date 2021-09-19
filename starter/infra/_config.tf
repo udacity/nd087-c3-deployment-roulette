8c05160161dcbaa5d31409f6ea269f622f1c53fd
Emmanuel Apau <e@mechanicode.io>
2021-09-19 09:42:02 -0400

    updated terraform config file

--- starter/infra/_config.tf
+++ starter/infra/_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-tf-emmanuel" # Update here with your S3 bucket
+    bucket = "udacity-tf-<first_name>" # Update here with your S3 bucket
     key    = "terraform/terraform.tfstate"
     region = "us-east-2"
   }
